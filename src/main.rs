7d2b9f7c45ef3b36e8dc9408dddba2fe0b399b70
River <22485304+actual-size@users.noreply.github.com>
2020-02-13 22:49:07 +1100

    Refactor in preparation for breaking calculations into functions

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,35 @@
 use std::f32::consts::PI;
 
 fn main() {
-    // let omega_zero: f32 =  0.949_546_379_547_515; // radians per sample @ 8kHz, freq = 1209 Hz
-    let omega_zero: f32 =  1336.0; // radians per sample @ 8kHz, freq = 1209 Hz
+    let target_freq: f32 =  1336.0; // in Hz
     let n = 80;
     let threshold = 1_000_000.0;
 
     let mut reader = hound::WavReader::open("testsamples/dtmf_112163_112196_11#9632_##9696.wav").unwrap();
     let sample_rate = reader.spec().sample_rate;
-    let mut sample_iter = reader.samples::<i16>();
+    let sample_iter = reader.samples::<i16>();
     let mut q0;
     let mut q1;
     let mut q2;
-    
-    'outer: loop {
-        let k = 0.5 + ((n as f32) * omega_zero) / (sample_rate as f32);
+    for (number_of_iterations, sample) in sample_iter.enumerate() {
+        // set up
+        let k = 0.5 + ((n as f32) * target_freq) / (sample_rate as f32);
         let w = (2.0 * PI / (n as f32))*k;
         let cosine = w.cos();
-        // let sine = w.sin();
         let coeff = 2.0 * cosine;
         q1 = 0.0;
         q2 = 0.0;
-        for _ in 0..n {
-            match sample_iter.next() {
-                None => break 'outer,
-                Some(i) => {
-                    q0 = coeff * q1 - q2 + i.unwrap() as f32;
-                    q2 = q1;
-                    q1 = q0;
-                }
-            }
-            
-        }
-        let magnitude_squared = q1*q1 + q2*q2 - q1 * q2 * coeff;
-        if magnitude_squared > threshold {
-            println!("{} exists {}", omega_zero, magnitude_squared);
+
+        // per sample calculation
+        q0 = coeff * q1 - q2 + (sample.unwrap() as f32);
+        q2 = q1;
+        q1 = q0;
+
+        // end of block
+        if number_of_iterations % n == 0 {
+            let magnitude_squared = q1*q1 + q2*q2 - q1 * q2 * coeff;
+            println!("{}Hz {} exist", target_freq, if magnitude_squared > threshold { "doesn't" } else { "does" });
+    
         }
     }
-    // let test = sample_iter.next().unwrap() as f64;
-    //                     .fold(0.0, |sqr_sum, s| {
-    //     let sample = s.unwrap() as f64;
-    //     sqr_sum + sample * sample
-    // })
-    // println!("RMS is {}", (sqr_sum / reader.len() as f64).sqrt());
 }
-
-
-/*
-
-k = (0.5 + (N * target_freq) / sample_rate) as i32
-w = (2*π/N)*k
-cosine = cos w
-sine = sin w
-coeff = 2 * cosine
-
-*/
